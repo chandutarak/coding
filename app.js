1c83bd2cba4b0b88c812f0bdffdf72185f681f79
chandutarak <152844237+chandutarak@users.noreply.github.com>
2023-12-11 11:05:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/gadgets', (request, response) => {
-  response.sendFile('/gadgets.html', {root: _dirname})
+  response.sendFile('/gadgets.html', {root: __dirname})
 })
 module.exports = app
